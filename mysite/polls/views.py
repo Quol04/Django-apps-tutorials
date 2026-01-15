d4efa4dca850f28979fbec460425cf7e2ae31d5e
quol <kuolamol77@gmail.com>
2026-01-15 17:59:06 +0300

    add the functional views for vote,details

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,3 +4,15 @@ from django.http import HttpResponse
 # Create your views here
 def index(request):
     return HttpResponse("Hello there, this is a polls app")
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at the resuts of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." %question_id)
+
